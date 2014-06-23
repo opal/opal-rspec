54152b7b2d29df19d9f73f6e2f029d9fc249f25c
Eric Watson <wasnotrice@gmail.com>
2014-06-23 12:02:43 -0500

    Add failing spec for named subject
    subject and named_subject should be the same object

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -87,10 +87,14 @@ describe "subject" do
 end
 
 describe "named subject" do
-  subject(:array) { [1, 2, 3] }
+  subject(:named_subject) { [1, 2, 3] }
 
   it "should be the subject" do
     subject.should be_kind_of(Array)
+  end
+
+  it "should be the named subject" do
+    subject.should eql(named_subject)
   end
 end
 
