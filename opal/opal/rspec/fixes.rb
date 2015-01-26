3172c75dd314591583c8a6676479a120fc18646a
Elia Schito <elia@schito.me>
2015-01-27 00:32:23 +0100

    Use correct ruby syntax for sub/gsub substititions

--- opal/opal/rspec/fixes.rb
+++ opal/opal/rspec/fixes.rb
@@ -10,9 +10,9 @@ module RSpec
   module Matchers
     module Pretty
       def underscore(camel_cased_word)
-        word = camel_cased_word.to_s.dup
-        word = word.gsub(/([A-Z]+)([A-Z][a-z])/,'$1_$2')
-        word = word.gsub(/([a-z\d])([A-Z])/,'$1_$2')
+        word = camel_cased_word.to_s
+        word = word.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+        word = word.gsub(/([a-z\d])([A-Z])/,'\1_\2')
         word = word.tr("-", "_")
         word = word.downcase
         word
@@ -35,7 +35,7 @@ module RSpec::ExampleGroups
 
     # Ruby requires first const letter to be A-Z. Use `Nested`
     # as necessary to enforce that.
-    name = name.gsub(/^([^A-Z]|$)/, 'Nested$1')
+    name = name.gsub(/^([^A-Z]|$)/, 'Nested\1')
 
     name
   end
