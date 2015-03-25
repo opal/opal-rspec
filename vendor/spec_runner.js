e8b1607cce82bb282de502fa07de83acff0445d1
Elia Schito <elia@schito.me>
2015-03-25 13:58:13 +0100

    Avoid phantomjs warning messages

--- vendor/spec_runner.js
+++ vendor/spec_runner.js
@@ -14,17 +14,26 @@ page.onInitialized = function() {
   });
 };
 
+/*
+ * Exit phantom instance "safely" see - https://github.com/ariya/phantomjs/issues/12697
+ * https://github.com/nobuoka/gulp-qunit/commit/d242aff9b79de7543d956e294b2ee36eda4bac6c
+ */
+function phantom_exit(code) {
+  page.close();
+  setTimeout(function () { phantom.exit(code); }, 0);
+}
+
 page.open(args[0], function(status) {
   if (status !== 'success') {
     console.error("Cannot load: " + args[0]);
-    phantom.exit(1);
+    phantom_exit(1);
   } else {
     var timeout = parseInt(args[1] || 60000, 10);
     var start = Date.now();
     var interval = setInterval(function() {
       if (Date.now() > start + timeout) {
         console.error("Specs timed out");
-        phantom.exit(124);
+        phantom_exit(124);
       } else {
         var code = page.evaluate(function() {
           return window.OPAL_SPEC_CODE;
@@ -32,7 +41,7 @@ page.open(args[0], function(status) {
 
         if (code === 0 || code === 1) {
           clearInterval(interval);
-          phantom.exit(code);
+          phantom_exit(code);
         }
       }
     }, 500);
