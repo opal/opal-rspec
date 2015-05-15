f46e87af98a4059e4a6f4c5af73317a2fbb23fc9
Brady Wied <brady@wied.us>
2015-05-15 10:06:05 -0600

    Clarify and use logic that makes more sense

--- opal/opal/rspec/fixes.rb
+++ opal/opal/rspec/fixes.rb
@@ -117,10 +117,10 @@ end
 
 class RSpec::Core::Reporter
   # https://github.com/opal/opal/issues/858
-  # The problem is not directly related to the Reporter class (it has more to do with Formatter's call in add using a splat in the args list and right now, Opal does not run a to_a on a splat before calling)
+  # The problem is not directly related to the Reporter class (it has more to do with Formatter's call in add using a splat in the args list and right now, Opal does not run a to_a on a splat before the callee method takes over)
   def register_listener(listener, *notifications)
-    # Unpack the splat
-    notifications = notifications[0].to_a if notifications[0].is_a? Set
+    # Unpack the splat unless it's already an array
+    notifications = notifications[0].to_a unless notifications[0].is_a? Array
     notifications.each do |notification|
       @listeners[notification.to_sym] << listener
     end
