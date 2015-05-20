62119f7d211a20b3732fe563e6d41c033983f268
Elia Schito <elia@schito.me>
2015-05-21 01:38:23 +0200

    Remove duplicated code (probably result of merge)
    e2290d56ff5147 (Skip diffing, Diff::LCS heavily uses String#<<)

--- opal/opal/rspec/fixes.rb
+++ opal/opal/rspec/fixes.rb
@@ -96,25 +96,6 @@ RSpec::Core::Formatters::DeprecationFormatter::GeneratedDeprecationMessage.class
   end
 end
 
-def (RSpec::Expectations).fail_with(message, expected=nil, actual=nil)
-  if !message
-    raise ArgumentError, "Failure message is nil. Does your matcher define the " +
-                         "appropriate failure_message_for_* method to return a string?"
-  end
-
-  if actual && expected
-    if all_strings?(actual, expected)
-      if any_multiline_strings?(actual, expected)
-        message # + "\nDiff:" + differ.diff_as_string(coerce_to_string(actual), coerce_to_string(expected))
-      end
-    elsif no_procs?(actual, expected) && no_numbers?(actual, expected)
-      message # + "\nDiff:" + differ.diff_as_object(actual, expected)
-    end
-  end
-
-  raise(RSpec::Expectations::ExpectationNotMetError.new(message))
-end
-
 class RSpec::Core::Reporter
   # https://github.com/opal/opal/issues/858
   # The problem is not directly related to the Reporter class (it has more to do with Formatter's call in add using a splat in the args list and right now, Opal does not run a to_a on a splat before the callee method takes over)
@@ -241,9 +222,16 @@ def (RSpec::Expectations).fail_with(message, expected=nil, actual=nil)
     raise ArgumentError, "Failure message is nil. Does your matcher define the " +
                          "appropriate failure_message_for_* method to return a string?"
   end
-  
-  # diff = differ.diff(actual, expected)
-  # message = "#{message}\nDiff:#{diff}" unless diff.empty?
+
+  if actual && expected
+    if all_strings?(actual, expected)
+      if any_multiline_strings?(actual, expected)
+        message # + "\nDiff:" + differ.diff_as_string(coerce_to_string(actual), coerce_to_string(expected))
+      end
+    elsif no_procs?(actual, expected) && no_numbers?(actual, expected)
+      message # + "\nDiff:" + differ.diff_as_object(actual, expected)
+    end
+  end
 
   raise(RSpec::Expectations::ExpectationNotMetError.new(message))
 end
