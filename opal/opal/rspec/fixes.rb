4ad054c6cf98ae193b9a8487044f2eee6d3c4a3d
Elia Schito <elia@schito.me>
2015-05-21 00:52:40 +0200

    Avoid string mutating methods in deprecation warn.
    fixes #20

--- opal/opal/rspec/fixes.rb
+++ opal/opal/rspec/fixes.rb
@@ -78,3 +78,18 @@ module RSpec
     end
   end
 end
+
+RSpec::Core::Formatters::DeprecationFormatter::GeneratedDeprecationMessage.class_eval do
+  def to_s
+    msg =  "#{@data[:deprecated]} is deprecated."
+    msg += " Use #{@data[:replacement]} instead." if @data[:replacement]
+    msg += " Called from #{@data[:call_site]}." if @data[:call_site]
+    msg
+  end
+
+  def too_many_warnings_message
+    msg = "Too many uses of deprecated '#{type}'."
+    msg += " Set config.deprecation_stream to a File for full output."
+    msg
+  end
+end
