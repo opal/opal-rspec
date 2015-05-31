29edf0e1889687dcd728be1a927d69e6ee0e9bcc
Brady Wied <brady@wied.us>
2015-05-31 17:54:39 -0600

    * Incorporate most of the hooks of the async_example run method, now summary results reflect failures properly * Clean up puts statements

--- opal/opal/rspec/async/async_example.rb
+++ opal/opal/rspec/async/async_example.rb
@@ -6,59 +6,46 @@ class Opal::RSpec::AsyncExample < ::RSpec::Core::Example
 
     start(reporter)
     ::RSpec::Core::Pending.mark_pending!(self, pending) if pending?
-
-    begin
-      if skipped?
-        ::RSpec::Core::Pending.mark_pending! self, skip
-      elsif !::RSpec.configuration.dry_run?
-        with_around_example_hooks do
-          begin
-            run_before_example
-            example_scope = self        
-            wrapped_block = lambda do |example|
-              done = lambda do
-                test_exception = @@async_exception != :do_not_throw ? @@async_exception : nil
-                if test_exception
-                  # exception needs to be set before calling finish
-                  example_scope.set_exception test_exception
-                  # reset
-                  @@async_exception = :do_not_throw
-                end
-                example_scope.finish(reporter)
-                promise.resolve test_exception == nil
-              end
-              @@async_exception = :do_not_throw
-              self.instance_exec(done, example, &example_scope.instance_variable_get(:@example_block))
+    
+    if skipped?
+      ::RSpec::Core::Pending.mark_pending! self, skip
+    elsif !::RSpec.configuration.dry_run?
+      # TODO: around example needs to be async
+      with_around_example_hooks do          
+        run_before_example
+        # Our wrapped block will execute with self == the group, not as the example, so we need to hold onto this for our promise resolve
+        example_scope = self        
+        wrapped_block = lambda do |example|
+          done = lambda do
+            if @@async_exception
+              # exception needs to be set before calling finish so results are correct
+              example_scope.set_exception @@async_exception
             end
-            
-            @example_group_instance.instance_exec(self, &wrapped_block)
-
-            if pending?
-              ::RSpec::Core::Pending.mark_fixed! self
-
-              raise ::RSpec::Core::Pending::PendingExampleFixedError,
-                    'Expected example to fail since it is pending, but it passed.',
-                    [location]
+            example_scope.run_after_example
+            # Using run method parameter here since self != the example (see above)
+            example_group_instance.instance_variables.each do |ivar|
+              example_group_instance.instance_variable_set(ivar, nil)
             end
-          rescue ::RSpec::Core::Pending::SkipDeclaredInExample
-            # no-op, required metadata has already been set by the `skip`
-            # method.
-          rescue Exception => e
-            set_exception(e)
-          ensure
-            run_after_example
+            example_scope.instance_variable_set(:@example_group_instance, nil)
+            example_scope.finish(reporter)              
+            ::RSpec.current_example = nil
+            promise.resolve @@async_exception == nil
           end
+          @@async_exception = nil
+          self.instance_exec(done, example, &example_scope.instance_variable_get(:@example_block))
         end
+        
+        @example_group_instance.instance_exec(self, &wrapped_block)
+
+        if pending?
+          ::RSpec::Core::Pending.mark_fixed! self
+
+          raise ::RSpec::Core::Pending::PendingExampleFixedError,
+                'Expected example to fail since it is pending, but it passed.',
+                [location]
+        end        
       end
-    rescue Exception => e
-      set_exception(e)
-    ensure
-      @example_group_instance.instance_variables.each do |ivar|
-        @example_group_instance.instance_variable_set(ivar, nil)
-      end
-      @example_group_instance = nil
     end
-  ensure
-    ::RSpec.current_example = nil
+    promise
   end
 end
