e1be616e2056e1c72eb77e22b95d39b307759164
Brady Wied <brady@wied.us>
2015-06-01 01:20:44 -0600

    Got async subject resolve working

---
+++ opal/opal/rspec/async/subject.rb
@@ -0,0 +1,12 @@
+class ::RSpec::Core::ExampleGroup
+  def self.async_it(the_subject=nil, &block)
+    wrapped_block = lambda do
+      promise = the_subject || subject
+      promise.then do |resolved_subject|
+        __memoized[:subject] = resolved_subject
+        self.instance_eval(&block)
+      end
+    end
+    async &wrapped_block
+  end  
+end
