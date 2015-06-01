ef7b2eff218a18cece537d36722f8143bfa77977
Brady Wied <brady@wied.us>
2015-06-01 00:04:06 -0600

    More modularity / less stuff in 1 file

--- opal/opal/rspec/async/async_example.rb
+++ opal/opal/rspec/async/async_example.rb
@@ -1,25 +1,3 @@
-class ::RSpec::Core::Example
-  def with_around_example_hooks(&block)
-    if around_example_hooks.empty?
-      yield
-    else
-      # Async examples will return a promise for the around hook to use after calling example.run
-      # Sync examples don't. Rather than monkey patch that method, which is more key to RSpec, monkey patching this, which is a smaller method
-      use_block = if self.is_a? Opal::RSpec::AsyncExample
-        block
-      else
-        lambda do
-          block.call          
-          Promise.new.resolve
-        end
-      end
-      @example_group_class.hooks.run(:around, :example, self, Procsy.new(self, &use_block))
-    end
-  rescue Exception => e
-    set_exception(e, "in an `around(:example)` hook")
-  end
-end
-
 class Opal::RSpec::AsyncExample < ::RSpec::Core::Example
   def notify_async_exception(exception)
     @async_exceptions << exception
