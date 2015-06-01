fd4a8bf5f96aa263d782a8401ed4c590c8209f7c
Brady Wied <brady@wied.us>
2015-05-31 20:05:45 -0600

    Got something working for around blocks

--- spec/async_spec.rb
+++ spec/async_spec.rb
@@ -8,18 +8,24 @@ describe "Asynchronous helpers" do
   
   before :all do
     @@around_testing = []
+    @@around_failures = []
   end
   
   around do |example|
     look_for = example.description
     @@around_testing << look_for
-    example.run
-    last = @@around_testing.pop   
-    raise "Around hook kept executing even though test #{@test_in_progress} was running!" if @test_in_progress
-    raise "Around hooks are messed up because we expected #{look_for} but we popped off #{last}" unless last == look_for
+    # Result is the result of the test, aka the promised value from run (true if success, false if failed)
+    # The complete_promise is needed so the runner knows when to continue
+    example.run.then do |result, complete_promise|
+      last = @@around_testing.pop
+      @@around_failures << "Around hook kept executing even though test #{@test_in_progress} was running!" if @test_in_progress
+      @@around_failures << "Around hooks are messed up because we expected #{look_for} but we popped off #{last}" unless last == look_for
+      complete_promise.resolve
+    end    
   end
   
   after :all do
+    raise @@around_failures.join "\n" if @@around_failures.any?
     raise 'hooks not empty!' unless @@around_testing.empty?
   end
   
