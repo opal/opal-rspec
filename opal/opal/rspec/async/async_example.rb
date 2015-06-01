bf45e1147a42f527066c841ec45d5aea81994e65
Brady Wied <brady@wied.us>
2015-05-31 22:06:00 -0600

    Have non-async examples return a resolved promise so that arounds can be consistent

--- opal/opal/rspec/async/async_example.rb
+++ opal/opal/rspec/async/async_example.rb
@@ -1,3 +1,26 @@
+class ::RSpec::Core::Example
+  def with_around_example_hooks(&block)
+    if around_example_hooks.empty?
+      yield
+    else
+      # Async examples will return a promise for the around hook to use after calling example.run
+      # Sync examples don't. Rather than monkey patch that method, which is more key to RSpec, monkey patching this, which is a smaller method
+      use_block = if self.is_a? Opal::RSpec::AsyncExample
+        block
+      else
+        lambda do
+          block.call          
+          Promise.new.resolve
+        end
+      end
+      @example_group_class.hooks.run(:around, :example, self, Procsy.new(self, &use_block))
+    end
+  rescue Exception => e
+    set_exception(e, "in an `around(:example)` hook")
+  end
+end
+
+
 class Opal::RSpec::AsyncExample < ::RSpec::Core::Example 
   def notify_async_completed
     @done.call
@@ -17,7 +40,6 @@ class Opal::RSpec::AsyncExample < ::RSpec::Core::Example
       ::RSpec.current_example = nil
       promise.resolve result
     elsif !::RSpec.configuration.dry_run?
-      # TODO: around example needs to be async
       with_around_example_hooks do
         around_promise_begin = Promise.new
         run_before_example
