1fb7a9f1f27bf9376cad6b938c7af4b787fcb428
Brady Wied <brady@wied.us>
2015-06-01 01:23:37 -0600

    Don't forget this

--- opal/opal/rspec/async/subject.rb
+++ opal/opal/rspec/async/subject.rb
@@ -1,4 +1,5 @@
 class ::RSpec::Core::ExampleGroup
+  # TODO: Move this to another file
   def self.async_it(the_subject=nil, &block)
     wrapped_block = lambda do
       promise = the_subject || subject
