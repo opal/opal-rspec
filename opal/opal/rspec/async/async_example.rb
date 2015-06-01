796822586cffd6bc09d778852afe654099ae36ca
Brady Wied <brady@wied.us>
2015-05-31 18:36:18 -0600

    Acknowledge that multiple queued up exceptions can happen, grab the first one for the results, and test for it using the old "should make example fail before async block reached" test

--- opal/opal/rspec/async/async_example.rb
+++ opal/opal/rspec/async/async_example.rb
@@ -23,13 +23,14 @@ class Opal::RSpec::AsyncExample < ::RSpec::Core::Example
             if example_scope.pending?
               ::RSpec::Core::Pending.mark_fixed! example_scope
 
-              @@async_exception = ::RSpec::Core::Pending::PendingExampleFixedError,
+              @@async_exceptions << ::RSpec::Core::Pending::PendingExampleFixedError.new(
                     'Expected example to fail since it is pending, but it passed.',
-                    [example_scope.location]
+                    [example_scope.location])
             end            
-            if @@async_exception
+            if @@async_exceptions.any?
               # exception needs to be set before calling finish so results are correct
-              example_scope.set_exception @@async_exception
+              # the first test to fail should be the one reported
+              example_scope.set_exception @@async_exceptions.first
             end
             example_scope.run_after_example
             # Using run method parameter here since self != the example (see above)
@@ -41,7 +42,7 @@ class Opal::RSpec::AsyncExample < ::RSpec::Core::Example
             ::RSpec.current_example = nil
             promise.resolve result
           end
-          @@async_exception = nil
+          @@async_exceptions = []
           self.instance_exec(done, example, &example_scope.instance_variable_get(:@example_block))
         end
         
--- spec/async_spec.rb
+++ spec/async_spec.rb
@@ -5,16 +5,16 @@ describe "Asynchronous helpers" do
     @model = Object.new
   end
   
-  # async "can run examples async" do |done|
-  #   1.should == 1
-  #   done.call
-  # end
-  #
-  # async "can access let() helpers and before() helpers" do |done|
-  #   foo.should eq(100)
-  #   @model.should be_kind_of(Object)
-  #   done.call
-  # end
+  async "can run examples async" do |done|
+    1.should == 1
+    done.call
+  end
+
+  async "can access let() helpers and before() helpers" do |done|
+    foo.should eq(100)
+    @model.should be_kind_of(Object)
+    done.call
+  end
 
   async "can finish running after a long delay and fail" do |done|
     obj = [1, 2, 3, 4]
@@ -24,7 +24,7 @@ describe "Asynchronous helpers" do
       done.call
     end
   end
-  
+
   async "can finish running after a long delay and succeed" do |done|
     obj = [1, 2, 3, 4]
 
@@ -32,8 +32,8 @@ describe "Asynchronous helpers" do
       obj.should == [1, 2, 3, 4]
       done.call
     end
-  end  
-  
+  end
+
   async 'skipped via variable', skip: true do |done|
     obj = [1, 2, 3, 4]
 
@@ -42,7 +42,7 @@ describe "Asynchronous helpers" do
       done.call
     end
   end
-  
+
   xasync 'skipped via xasync' do |done|
     obj = [1, 2, 3, 4]
 
@@ -51,15 +51,15 @@ describe "Asynchronous helpers" do
       done.call
     end
   end
-  
+
   # async 'skipped in example' do |done|
   #   skip
   # end
-  
+
   # async 'pending in example' do |done|
   #   pending 'not ready yet'
   # end
-  
+
   async 'pending via variable', pending: 'the reason' do |done|
     obj = [1, 2, 3, 4]
 
@@ -69,16 +69,16 @@ describe "Asynchronous helpers" do
     end
   end
   
-  # TODO: test descendants
-  #
-  #
-  # async "should make example fail before async block reached" do
-  #   expect {
-  #     expect(:foo).to eq(:baz)
-  #   }.to raise_error(Exception)
-  #
-  #   delay(0) do
-  #     async { expect(42).to eq(42) }
-  #   end
-  # end
+  # TODO: test example group descendants, around blocks
+  
+  # TODO, how to test this now? Right now, manually looking and ensuring failure message shows foo/baz and not 42/43
+  async "should make example fail before async block reached" do |done|
+    # Can't say "should raise exception" around an expectation anymore since expectations don't throw
+    expect(:foo).to eq(:baz)
+
+    delay(0) do
+      expect(42).to eq(43)
+      done.call
+    end
+  end
 end
