5e32d8389ddc9473e7947df07c242708455c56d3
Brady Wied <brady@wied.us>
2015-06-02 12:20:52 -0600

    Split pending/skip into 2 describes

--- spec/skip_pending_spec.rb
+++ spec/skip_pending_spec.rb
@@ -1,9 +1,31 @@
-describe 'skip/pending' do
+describe 'skip' do
   it 'in example, no promise' do
     skip 'want to skip within'
   end
   
-  it 'in example without a promise' do
+  it 'no implementation provided'
+  
+  skip 'entire group' do
+    it 'example 1' do
+      1.should == 2
+    end
+    
+    it 'example 2' do
+      1.should == 3
+    end
+  end
+  
+  xit 'via xit' do
+    1.should == 3
+  end
+  
+  it 'via variable', skip: true do
+    1.should == 3
+  end
+end
+
+describe 'pending' do
+  it 'in example, no promise' do
     obj = [1, 2, 3, 4]
     obj.should == [2, 2, 3, 4]
     pending 'want to pend within'
