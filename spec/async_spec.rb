c14da5a85cfd5baf1aea10f453ab4865aac78127
Brady Wied <brady@wied.us>
2015-06-02 09:22:07 -0600

    Further split out the async spec to make things more maintainable

--- spec/async_spec.rb
+++ spec/async_spec.rb
@@ -1,37 +1,5 @@
-describe "Asynchronous helpers" do
-  let(:foo) { 100 }
-
-  before do
-    @model = Object.new
-    @test_in_progress = nil
-  end
-  
-  before :all do
-    @@around_testing = []
-    @@around_failures = []
-  end
-  
-  around do |example|
-    look_for = example.description
-    @@around_testing << look_for
-    # Result is the result of the test, aka the promised value from run (true if success, false if failed)
-    # The complete_promise is needed so the runner knows when to continue
-    example.run.then do |result, complete_promise|
-      last = @@around_testing.pop
-      @@around_failures << "Around hook kept executing even though test #{@test_in_progress} was running!" if @test_in_progress
-      @@around_failures << "Around hooks are messed up because we expected #{look_for} but we popped off #{last}" unless last == look_for
-      complete_promise.resolve
-    end    
-  end
-  
-  after :all do
-    raise @@around_failures.join "\n" if @@around_failures.any?
-    raise 'hooks not empty!' unless @@around_testing.empty?
-  end
-    
-  it 'works with a sync test in a group of async tests with an around hook' do
-    1.should == 1
-  end
+describe 'Promise examples' do
+  let(:foo) { 100 }    
   
   context 'promise returned by example' do
     it 'matcher fails properly' do
@@ -80,17 +48,13 @@ describe "Asynchronous helpers" do
       end
     end
 
-    xit 'via xasync' do
+    xit 'via xit' do
       obj = [1, 2, 3, 4]
 
       delay_with_promise 1 do
         obj.should == [2, 2, 3, 4]
       end
-    end
-
-    it 'in example, no promise' do
-      skip 'want to skip within'
-    end
+    end    
   
     it 'in example, inside promise' do
       delay_with_promise 1 do
@@ -107,13 +71,7 @@ describe "Asynchronous helpers" do
   end
   
   context 'pending' do
-    it 'in example without a promise' do
-      obj = [1, 2, 3, 4]
-      obj.should == [2, 2, 3, 4]
-      pending 'want to pend within'
-    end
-
-    it 'in example with a promise' do
+    it 'in example' do
       obj = [1, 2, 3, 4]
 
       delay_with_promise(1) do
@@ -129,11 +87,9 @@ describe "Asynchronous helpers" do
         obj.should == [2, 2, 3, 4]
       end
     end    
-  end
-  
-  # TODO, how to test this now? Right now, manually looking and ensuring failure message shows foo/baz and not 42/43
+  end  
+
   it "should make example fail properly before async block reached" do
-    # Can't say "should raise exception" around an expectation anymore since expectations don't throw
     expect(:foo).to eq(:baz)
 
     delay_with_promise(0) do
@@ -154,6 +110,14 @@ end
 describe 'async/sync mix' do
   it 'fails properly if a sync test is among async tests' do
     1.should == 2
+  end
+  
+  it 'is an async test between 2 sync tests' do
+    p = delay_with_promise 0 do
+      1.should == 1
+    end
+    puts "promise we created is #{p}"
+    p
   end
 
   it 'passes correctly if a sync test is among async tests' do
