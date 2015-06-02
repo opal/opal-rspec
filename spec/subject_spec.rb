ae2ffb5f3d1c4acb0685601014165717107567b9
Brady Wied <brady@wied.us>
2015-06-02 15:19:52 -0600

    Migrate these into the same spec file

--- spec/subject_spec.rb
+++ spec/subject_spec.rb
@@ -1,21 +1,35 @@
-describe 'subject' do
+describe 'subject' do    
   context 'sync' do
-    subject { 42 }
-    
-    context 'passes' do
-      it { is_expected.to eq 42 }
+    context 'named' do
+      subject(:named_subject) { [1, 2, 3] }
+
+      it "should be the subject" do
+        subject.should be_kind_of(Array)
+      end
+
+      it "should be the named subject" do
+        subject.should eql(named_subject)
+      end     
     end
     
-    context 'assertion fails properly' do
-      it { is_expected.to eq 43 }
-    end
+    context 'unnamed' do
+      subject { 42 }
     
-    context 'fails properly during subject create' do
-      subject do
-        raise 'did not work'
+      context 'passes' do
+        it { is_expected.to eq 42 }
       end
     
-      it { is_expected.to eq 42 }
+      context 'assertion fails properly' do
+        it { is_expected.to eq 43 }
+      end
+    
+      context 'fails properly during subject create' do
+        subject do
+          raise 'did not work'
+        end
+    
+        it { is_expected.to eq 42 }
+      end      
     end
   end
   
