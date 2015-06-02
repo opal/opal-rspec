2d0f45fb47364beb34812ce1805c2e7672450e51
Brady Wied <brady@wied.us>
2015-06-01 18:52:54 -0600

    Carve out separate test for these

--- spec/async_spec.rb
+++ spec/async_spec.rb
@@ -242,85 +242,61 @@ describe 'async subject' do
   end
 end
 
-describe 'sync before' do
-  before do
-    @test_value = 42
-  end
-  
-  subject { 42 } 
-  
-  context 'succeeds' do
-    it { is_expected.to eq @test_value }
-  end
-  
-  context 'before fails properly' do
-    before do
-      raise 'something did not work right'
-    end
-    
-    it { is_expected.to eq @test_value }
-  end
-  
-  context 'match fails properly' do
-    it { is_expected.to_not eq @test_value }
-  end
-end
-
-describe 'async before' do  
-  context 'sync subject' do
-    before do      
-      promise = Promise.new
-      # self/instance variable will not work inside delay
-      set_test_val = lambda {|v| @test_value = v}
-      delay 1 do
-        set_test_val[42]
-        promise.resolve
-      end
-      promise
-    end
-    
-    subject { 42 } 
-    
-    context 'succeeds' do
-      async_it { is_expected.to eq @test_value }      
-    end
-    
-    context 'before fails properly' do
-      pending 'write this'
-    end
-    
-    context 'match fails properly' do
-      pending 'write this'
-    end
-  end
-  
-  context 'async subject' do
-    subject do
-      promise = Promise.new
-      delay 1 do      
-        promise.resolve 42
-      end
-      promise
-    end
-    
-    context 'both succeed' do
-      pending 'write this'
-    end
-    
-    context 'both fail properly' do
-      pending 'write this'
-    end
-    
-    context 'before succeeds, assertion fails properly' do
-      pending 'write this'
-    end
-    
-    context 'before fails properly' do
-      pending 'write this'
-    end
-    
-    context 'match fails properly' do
-      pending 'write this'
-    end
-  end
-end
+# describe 'async before' do
+#   context 'sync subject' do
+#     before do
+#       promise = Promise.new
+#       # self/instance variable will not work inside delay
+#       set_test_val = lambda {|v| @test_value = v}
+#       delay 1 do
+#         set_test_val[42]
+#         promise.resolve
+#       end
+#       promise
+#     end
+#
+#     subject { 42 }
+#
+#     context 'succeeds' do
+#       async_it { is_expected.to eq @test_value }
+#     end
+#
+#     context 'before fails properly' do
+#       pending 'write this'
+#     end
+#
+#     context 'match fails properly' do
+#       pending 'write this'
+#     end
+#   end
+#
+#   context 'async subject' do
+#     subject do
+#       promise = Promise.new
+#       delay 1 do
+#         promise.resolve 42
+#       end
+#       promise
+#     end
+#
+#     context 'both succeed' do
+#       pending 'write this'
+#     end
+#
+#     context 'both fail properly' do
+#       pending 'write this'
+#     end
+#
+#     context 'before succeeds, assertion fails properly' do
+#       pending 'write this'
+#     end
+#
+#     context 'before fails properly' do
+#       pending 'write this'
+#     end
+#
+#     context 'match fails properly' do
+#       pending 'write this'
+#     end
+#   end
+# end
