643d9bb36dacb242c35e5ac0e8bb995e42f1e570
Brady Wied <brady@wied.us>
2015-06-03 14:18:42 -0600

    In order to make maintenance easier, make it easier to add around hooks, and after better promise understanding, do a more literal conversion of begins/rescues/ensures to promises

--- opal/opal/rspec/async/async_example.rb
+++ opal/opal/rspec/async/async_example.rb
@@ -1,53 +1,12 @@
 class ::RSpec::Core::Example  
-  def notify_async_completed(reporter, exception=nil)
-    puts "notify_async_completed called with exception #{exception}"
-    Promise.value.then do
-      Promise.value.then do
-        if exception
-          unless exception.is_a? Pending::SkipDeclaredInExample
-            puts 'got an exception, noting it'
-            # exception needs to be set before calling finish so results are correct
-            # the first test to fail should be the one reported
-            set_exception exception
-          end            
-        else
-          if pending?
-            puts 'found pending example that did not fail!!!'
-            ::RSpec::Core::Pending.mark_fixed! self
-
-            set_exception ::RSpec::Core::Pending::PendingExampleFixedError.new(
-                  'Expected example to fail since it is pending, but it passed.',
-                  [location])
-          end        
-        end
-      end.ensure do
-        run_after_example
-      end
-    end.rescue do |e|
-      set_exception e
-    end.ensure do
-      @example_group_instance.instance_variables.each do |ivar|
-        @example_group_instance.instance_variable_set(ivar, nil)
-      end  
-      @example_group_instance = nil
-      
-      result = finish(reporter)     
-      puts "----- example complete #{metadata[:description]} with result #{result} ------"
-      ::RSpec.current_example = nil
-      result
-    end     
-  end
-  
-  def core_block_run(reporter)
+  def core_block_run
     example_promise = Promise.value(@example_group_instance.instance_exec(self, &@example_block))
     example_promise.then do |result|
-      puts 'notifying completed'
-      notify_async_completed reporter      
+      result      
     end.rescue do |ex|
       ex ||= Exception.new 'Async promise failed for unspecified reason'
-      ex = Exception.new ex unless ex.kind_of?(Exception)          
-      puts "notifying example exception #{ex}"
-      notify_async_completed reporter, ex      
+      ex = Exception.new ex unless ex.kind_of?(Exception)
+      ex
     end
   end
   
@@ -73,28 +32,62 @@ class ::RSpec::Core::Example
     end    
   end
   
+  # TODO: Fix this
+  def with_around_example_hooks
+    yield
+  end 
+  
   def run(example_group_instance, reporter)
-    puts "----- example begin #{metadata[:description]} ------"    
     @example_group_instance = example_group_instance
-    ::RSpec.current_example = self
+    RSpec.current_example = self
 
     start(reporter)
-    ::RSpec::Core::Pending.mark_pending!(self, pending) if pending?    
+    Pending.mark_pending!(self, pending) if pending?
 
-    if skipped?
-      ::RSpec::Core::Pending.mark_pending! self, skip
-      result = finish(reporter)              
-      ::RSpec.current_example = nil
-      return Promise.value result
-    elsif !::RSpec.configuration.dry_run?
-      # TODO: Put around back in here      
-      run_before_example.then do
-        resolve_subject         
-      end.then do
-        core_block_run reporter          
-      end.fail do |ex|
-        notify_async_completed(reporter, ex)
+    Promise.value.then do
+      Promise.value.then do
+        if skipped?
+          Pending.mark_pending! self, skip
+        elsif !RSpec.configuration.dry_run?
+          with_around_example_hooks do
+            Promise.value.then do
+              run_before_example.then do
+                resolve_subject
+              end.then do
+                core_block_run
+              end.then do
+                if pending?
+                  Pending.mark_fixed! self
+
+                  raise Pending::PendingExampleFixedError,
+                        'Expected example to fail since it is pending, but it passed.',
+                        [location]
+                end
+              end
+            end.rescue do |e|
+              puts "got exception in run #{e}"
+              # no-op, required metadata has already been set by the `skip`
+              # method.              
+              unless e.is_a? Pending::SkipDeclaredInExample
+                set_exception(e)
+              end
+            end.ensure do
+              run_after_example
+            end            
+          end
+        end
+      end.rescue do |e|
+        set_exception(e)
+      end.ensure do
+        @example_group_instance.instance_variables.each do |ivar|
+          @example_group_instance.instance_variable_set(ivar, nil)
+        end
+        @example_group_instance = nil
       end      
+    end.then do
+      finish(reporter)
+    end.ensure do
+      RSpec.current_example = nil
     end
-  end
+  end  
 end
