e4d9dedb050f431b871a8951022a7f80a39c5ebb
Brady Wied <brady@wied.us>
2015-06-03 10:33:36 -0600

    Test before :context

--- spec/before_hooks_spec.rb
+++ spec/before_hooks_spec.rb
@@ -11,99 +11,36 @@ describe 'hooks' do
           set_test_val[42]
         end
       end
-      
-      context 'with sync subject' do
-        subject { 42 }
-
-        context 'succeeds' do
-          it { is_expected.to eq @test_value }
-        end
-
-        context 'before fails properly' do
-          let(:raise_before_error) { true }
-        
-          # Won't run, but needs to be here to make test go
-          it { is_expected.to eq @test_value }
-        end
-
-        context 'match fails properly' do
-          it { is_expected.to_not eq @test_value }
-        end
-        
-        context 'async match' do
-          it 'succeeds' do
-            delay_with_promise 0 do
-              expect(subject).to eq @test_value
-            end
-          end
-          
-          it 'fails properly' do
-            delay_with_promise 0 do
-              expect(subject).to_not eq @test_value
-            end
-          end
-        end
-      end
-      
-      context 'with async subject' do
-        let(:raise_before_subj_error) { false }
-        
-        subject do
-          raise_err = raise_before_subj_error
-          delay_with_promise 0 do
-            raise 'problem in subject' if raise_err
-            42
-          end
-        end
-
-        context 'both succeed' do
-          it { is_expected_to eq @test_value }
-        end
-
-        context 'both fail properly' do
-          let(:raise_before_error) { true }
-          let(:raise_before_subj_error) { true }
-          
-          # Won't run, but needs to be here to make test go
-          it { is_expected_to eq @test_value }
-        end
-
-        context 'before succeeds, assertion fails properly' do
-          it { is_expected_to_not eq @test_value }
-        end
-
-        context 'before fails properly' do
-          let(:raise_before_error) { true }
-          
-          # Won't run, but needs to be here to make test go
-          it { is_expected_to eq @test_value }
-        end
-
-        context 'before succeeds, subject fails properly' do
-          let(:raise_before_subj_error) { true }
-          
-          # Won't run, but needs to be here to make test go
-          it { is_expected_to eq @test_value }
-        end        
-        
-        context 'async match' do
-          it 'succeeds' do
-            delay_with_promise 0 do
-              expect(subject).to eq @test_value
-            end
-          end
-          
-          it 'fails properly' do
-            delay_with_promise 0 do
-              expect(subject).to_not eq @test_value
-            end
-          end
-        end
-      end
-    end    
-  
     context 'both sync' do  
-      subject { 42 } 
+      subject { 42 }
+      
+      context 'context' do
+        context 'success' do
+          before :context do
+            @@before_context_both_sync = 22
+          end
+        
+          before do
+            raise "@@before_context_both_sync should already be 22!" unless @@before_context_both_sync == 22
+            @test_value = 42
+          end
+        
+          it { is_expected.to eq @test_value }          
+        end
+        
+        context 'fails properly' do
+          before :context do
+            raise 'it failed!'
+            @@before_context_both_sync = 55
+          end
+        
+          before do
+            raise "before_context should have failed already!" if @@before_context_both_sync == 55
+          end
+        
+          it { is_expected.to eq @test_value }
+        end
+      end
   
       context 'succeeds' do
         before do
