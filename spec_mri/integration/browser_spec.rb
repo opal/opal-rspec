800f1a324f0aa365baaf06b23c6184fcb90c439c
Brady Wied <brady@bswtechconsulting.com>
2015-08-15 19:12:44 -0600

    * Didn't need synchronize after all for Firefox, so go back to smarter Capybara way (and increase the cushion) * Don't really need to check JS errors, final result is what matters

--- spec_mri/integration/browser_spec.rb
+++ spec_mri/integration/browser_spec.rb
@@ -1,40 +1,27 @@
 require 'rspec'
 require 'capybara/rspec'
 require 'capybara-webkit'
+
+# Use Rack config exactly as shipped in the GEM
 Capybara.app = Rack::Builder.new_from_string(File.read('config.ru'))
-Capybara.register_driver :selenium do |app|
-  profile = Selenium::WebDriver::Firefox::Profile.new
-  # see https://github.com/mguillem/JSErrorCollector
-  profile.add_extension 'spec_mri/integration/JSErrorCollector.xpi'
-  Capybara::Selenium::Driver.new app, :profile => profile
-end
 
 describe 'browser formatter', type: :feature, js: true do
   RSpec.shared_examples :browser do
-    before do      
+    before do            
       visit '/'
-      # specs can take some time to finish and Capybara.default_wait_time didn't seem to work right with synchronize (see below)
-      sleep 20
+      # Specs should run in 12 seconds but in case Travis takes longer, provide some cushion
+      Capybara.default_wait_time = 40
     end
     
-    it 'has test results' do
-      # synchronize is needed to play nice with Selenium/Firefox
-      page.document.synchronize do
-        expect(page).to have_content '182 examples, 65 failures, 22 pending'
-      end
-    end
-    
-    it 'has expected JS errors' do
-      expect(js_errors).to be_empty
+    it 'matches test results' do
+      expect(page).to have_content '182 examples, 65 failures, 22 pending'
     end    
   end
   
   context 'Webkit' do
     before do
       Capybara.javascript_driver = :webkit
-    end
-    
-    let(:js_errors) { page.driver.error_messages }
+    end   
     
     include_examples :browser
   end
@@ -42,13 +29,7 @@ describe 'browser formatter', type: :feature, js: true do
   context 'Firefox' do
     before do
       Capybara.javascript_driver = :selenium
-    end
-        
-    let(:js_errors) do
-      raw = page.execute_script 'return window.JSErrorCollector_errors.pump()'
-      # Noise
-      raw.reject {|e| e['errorMessage'] == 'SyntaxError: unreachable code after return statement'}
-    end
+    end   
     
     include_examples :browser
   end
