620615da17c6388cde01b4faaf0093833d442f6e
Brady Wied <brady@bswtechconsulting.com>
2015-08-25 08:09:32 -0600

    No need for separate module here since this comes after the class

--- opal/opal/rspec/fixes/random.rb
+++ opal/opal/rspec/fixes/random.rb
@@ -1,13 +1,11 @@
 # Random causes problems that can lock up a browser (see README) 
-module ::RSpec::Core::Ordering
-  # Identity is usually the default, so borrowing its implementation, this forces 'accidental' random usage down that path
-  class Random
-    def initialize(configuration)
-      `console.warn("Random order is not currently supported by opal-rspec, using default order.")`
-    end
-    
-    def order(items)
-      items
-    end
-  end  
+class ::RSpec::Core::Ordering::Random
+  def initialize(configuration)
+    `console.warn("Random order is not currently supported by opal-rspec, using default order.")`
+  end
+  
+  # Identity is usually the default, so borrowing its implementation, this forces 'accidental' random usage down that path    
+  def order(items)
+    items
+  end
 end
