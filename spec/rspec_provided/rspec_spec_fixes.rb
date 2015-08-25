d07e226306f47e8ddcd4a4ea9e70e98a6667a3b1
Brady Wied <brady@bswtechconsulting.com>
2015-08-24 22:59:48 -0600

    * Explain what dynamic requires are * Mock more stuff * Exclude more incompatible specs

--- spec/rspec_provided/rspec_spec_fixes.rb
+++ spec/rspec_provided/rspec_spec_fixes.rb
@@ -1,3 +1,4 @@
+# dealing with dynamic requires
 require 'rspec/support'
 require 'rspec/support/spec/deprecation_helpers'
 require 'rspec/support/spec/with_isolated_stderr'
@@ -6,6 +7,7 @@ require 'rspec/support/spec/formatting_support'
 require 'rspec/support/spec/with_isolated_directory'
 require 'rspec/support/ruby_features'
 require 'support/shared_example_groups'
+
 # begin mocks
 class ::Dir
   def self.[](index)
@@ -34,3 +36,21 @@ RSpec.configure do |c|
   
 end
 # end RSpec config
+
+# Do not have the mathn standard library
+require 'support/formatter_support'
+module MathnIntegrationSupport
+  def with_mathn_loaded
+    yield
+  end
+end
+
+# Not in Opal's core lib
+class SecurityError < Exception; end
+
+# the safety method is defined in helper_methods and uses threads
+module RSpecHelpers
+  def safely
+    yield
+  end
+end
