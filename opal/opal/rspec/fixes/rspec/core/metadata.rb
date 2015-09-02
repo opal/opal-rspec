3d604d8f36cdc2f25c5a00a29ae46282b8814823
Brady Wied <brady@bswtechconsulting.com>
2015-09-01 23:43:18 -0600

    Can't use 'and' operator, needed to tweak this further

--- opal/opal/rspec/fixes/rspec/core/metadata.rb
+++ opal/opal/rspec/fixes/rspec/core/metadata.rb
@@ -3,10 +3,14 @@ class ::RSpec::Core::Metadata::ExampleGroupHash
     candidate = metadata[:description_args].first
     return candidate unless NilClass === candidate || String === candidate
     parent_group = metadata[:parent_example_group]
+    puts "pg candidate #{parent_group} is a #{parent_group.class}" if parent_group.is_a? Hash
     # https://github.com/opal/opal/issues/1090
     # parent_group && parent_group[:described_class]
-    result = parent_group and parent_group[:described_class]
-    # also need an explicit return here, leaving until the last line causes problems
-    result
+    # and didn't work either here (on MRI or on Opal)
+    if parent_group
+      parent_group[:described_class]
+    else
+      nil
+    end
   end
 end
