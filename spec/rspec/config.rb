0dec0445fa76d9bac6b7c16e711c5c080d910c69
Brady Wied <brady@bswtechconsulting.com>
2015-10-02 10:16:54 -0600

    Use the doc formatter for node as well

--- spec/rspec/config.rb
+++ spec/rspec/config.rb
@@ -1,4 +1,4 @@
 RSpec.configure do |c|
   # will make it easier to exclude certain specs
-  c.default_formatter = ::RSpec::Core::Formatters::DocumentationFormatter if Opal::RSpec::Runner.phantom?
+  c.default_formatter = ::RSpec::Core::Formatters::DocumentationFormatter if Opal::RSpec::Runner.non_browser?
 end
