ba8247120eb0a2546d22c324bbf4606d48505d16
Brady Wied <brady@bswtechconsulting.com>
2015-10-02 22:42:44 -0600

    * Forgot about pre-require fix * Remove debug message

--- opal/opal/rspec/fixes/opal/class.rb
+++ opal/opal/rspec/fixes/opal/class.rb
@@ -1,6 +1,5 @@
 # https://github.com/opal/opal/issues/1079, fixed in Opal 0.9
 unless Opal::RSpec::Compatibility.full_class_names?
-  puts 'monkey patching classes'
   class Class
     def to_s
       name || "#<#{`self.$$is_mod ? 'Module' : 'Class'`}:0x#{__id__.to_s(16)}>"
--- opal/opal/rspec/pre_require_fixes.rb
+++ opal/opal/rspec/pre_require_fixes.rb
@@ -4,4 +4,5 @@ RUBY_DESCRIPTION = RUBY_VERSION unless defined?(RUBY_DESCRIPTION)
 class SecurityError < Exception; end unless defined?(SecurityError)
 
 # This completely changes the struct class, so need it before RSpec runs
+require_relative 'fixes/opal/compatibility'
 require_relative 'fixes/opal/corelib/struct'
