389577f540b520ac6db1e7af20875b78082be95e
Brady Wied <brady@bswtechconsulting.com>
2015-10-15 15:32:37 -0600

    Opal 0.9 Travis Gemfile and encoding/metadata fix

--- opal/opal/rspec/fixes.rb
+++ opal/opal/rspec/fixes.rb
@@ -1,4 +1,4 @@
-require 'encoding'
+require 'encoding' unless Object.const_defined? :Encoding
 # Thread usage in core.rb
 require 'thread'
 require_relative 'fixes/rspec'
--- opal/opal/rspec/fixes/rspec/core/metadata.rb
+++ opal/opal/rspec/fixes/rspec/core/metadata.rb
@@ -1,17 +1,24 @@
-# https://github.com/opal/opal/issues/1090, fixed in Opal 0.9
-unless Opal::RSpec::Compatibility.and_works_with_lhs_nil?
-  class ::RSpec::Core::Metadata::ExampleGroupHash
-    def described_class
-      candidate = metadata[:description_args].first
-      return candidate unless NilClass === candidate || String === candidate
-      parent_group = metadata[:parent_example_group]
-      # https://github.com/opal/opal/issues/1090, fixed in Opal 0.9
-      # parent_group && parent_group[:described_class]
-      if parent_group
-        parent_group[:described_class]
-      else
-        nil
+module ::RSpec::Core::Metadata
+  # https://github.com/opal/opal/issues/1090, fixed in Opal 0.9
+  unless ::Opal::RSpec::Compatibility.and_works_with_lhs_nil?
+    class ExampleGroupHash
+      def described_class
+        candidate = metadata[:description_args].first
+        return candidate unless NilClass === candidate || String === candidate
+        parent_group = metadata[:parent_example_group]
+        # https://github.com/opal/opal/issues/1090, fixed in Opal 0.9
+        # parent_group && parent_group[:described_class]
+        if parent_group
+          parent_group[:described_class]
+        else
+          nil
+        end
       end
     end
+  end
+
+  # Can't get paths yet in Opal
+  def self.relative_path(line)
+    nil
   end
 end
