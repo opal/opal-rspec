8442253b7cc000b6b40d4eca9e6c5c962148f7d0
Brady Wied <brady@bswtechconsulting.com>
2015-10-17 21:44:40 -0600

    Move check here where proxies are created, best place

--- opal/opal/rspec/fixes/rspec/mocks/targets.rb
+++
@@ -1,8 +0,0 @@
-class ::RSpec::Mocks::TargetBase
-  OPAL_NON_MOCKABLE_TYPES = [String, Number]
-
-  def initialize(target)
-    raise "#{target.class} #{target} cannot be used for mocking in Opal!" if OPAL_NON_MOCKABLE_TYPES.include?(target.class)
-    @target = target
-  end
-end
