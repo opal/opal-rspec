2c7c82c35fd4497765a31d90fe5b5a04f047c6a0
Brady Wied <brady@bswtechconsulting.com>
2015-10-22 11:21:54 -0600

    Add arguments to promise value calls to ensure arity checks pass

--- opal/opal/rspec/async/hooks.rb
+++ opal/opal/rspec/async/hooks.rb
@@ -1,9 +1,9 @@
 class RSpec::Core::Hooks::HookCollection
   def run
-    hooks.inject(Promise.value) do |previous_hook_promise, next_hook|
+    hooks.inject(Promise.value(true)) do |previous_hook_promise, next_hook|
       previous_hook_promise.then do
         result = next_hook.run @example
-        Promise.value result
+        Promise.value(result)
       end
     end
   end
