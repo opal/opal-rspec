97aba5418aed9ca93b2d2ff0d671065a4d7af072
Brady Wied <brady@bswtechconsulting.com>
2016-03-05 15:22:20 -0700

    Remove async support for now

--- opal/opal/rspec.rb
+++ opal/opal/rspec.rb
@@ -3,7 +3,7 @@ require 'opal/rspec/requires'
 require 'opal/rspec/fixes'
 require 'opal/rspec/formatter/browser_formatter'
 require 'opal/rspec/runner'
-require 'opal/rspec/async'
+#require 'opal/rspec/async'
 
 RSpec.configure do |config|
   config.default_formatter = ::RSpec::Core::Runner.non_browser? ? ::RSpec::Core::Formatters::ProgressFormatter : ::Opal::RSpec::BrowserFormatter
@@ -19,5 +19,5 @@ RSpec.configure do |config|
   end
 
   # Legacy helpers
-  config.include Opal::RSpec::AsyncHelpers
+  #config.include Opal::RSpec::AsyncHelpers
 end
