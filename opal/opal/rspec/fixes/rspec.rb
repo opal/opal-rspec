39cd77206384671a5bca2bfabdd042e7492be46b
Brady Wied <brady@bswtechconsulting.com>
2016-03-05 15:20:46 -0700

    Doesn't work the same way anymore, remove for now

--- opal/opal/rspec/fixes/rspec.rb
+++ opal/opal/rspec/fixes/rspec.rb
@@ -1,6 +1,5 @@
 require_relative 'rspec/core'
 require_relative 'rspec/support'
-require_relative 'rspec/expectations'
 require_relative 'rspec/matchers'
 require_relative 'rspec/mocks'
 require_relative 'rspec/caller_filter'
