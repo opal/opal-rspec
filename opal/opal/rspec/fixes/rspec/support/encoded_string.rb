fa21fb044b693a7027fb69e03dbefc5b3dd52e20
Brady Wied <brady@bswtechconsulting.com>
2016-03-23 17:08:04 -0600

    New methods in encoded string that we need to work around

--- opal/opal/rspec/fixes/rspec/support/encoded_string.rb
+++ opal/opal/rspec/fixes/rspec/support/encoded_string.rb
@@ -3,8 +3,12 @@ class RSpec::Support::EncodedString
   def initialize(string, encoding=nil)
     @string = string
   end
-  
+
   def to_s
     @string
+  end
+
+  def matching_encoding(string)
+    string
   end
 end
