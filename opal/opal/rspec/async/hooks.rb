aaf88d95a8a0d7683ab0f77c4be03fd57b0c87ba
Brady Wied <brady@bswtechconsulting.com>
2016-03-23 21:21:23 -0600

    This is different now, try and address it

--- opal/opal/rspec/async/hooks.rb
+++ opal/opal/rspec/async/hooks.rb
@@ -1,8 +1,12 @@
-class RSpec::Core::Hooks::HookCollection
-  def run
-    hooks.inject(Promise.value(true)) do |previous_hook_promise, next_hook|
+class RSpec::Core::Hooks::HookCollections
+  def run_owned_hooks_for(position, scope, example_or_group)
+    # matching_hooks_for(position, scope, example_or_group).each do |hook|
+    #   hook.run(example_or_group)
+    # end
+    matching_hooks = matching_hooks_for(position, scope, example_or_group)
+    matching_hooks.inject(Promise.value(true)) do |previous_hook_promise, next_hook|
       previous_hook_promise.then do
-        result = next_hook.run @example
+        result = next_hook.run(example_or_group)
         Promise.value(result)
       end
     end
