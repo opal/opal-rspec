66f0f1d346ab08164efb699a182b3fd49057d71b
Brady Wied <brady@bswtechconsulting.com>
2016-04-20 17:33:28 -0600

    Sync up spec helper with rspec-support version, now 105 passing, 27 unexpected fails

--- spec/rspec/support/spec_helper.rb
+++ spec/rspec/support/spec_helper.rb
@@ -6,14 +6,16 @@ RSpec.configure do |c|
   c.include RSpecHelpers
   c.include RSpec::Support::WithIsolatedStdErr
   c.include RSpec::Support::FormattingSupport
+  # Opal - no forking based tests
+  # c.include RSpec::Support::InSubProcess
 
   unless defined?(Debugger) # debugger causes warnings when used
     c.before do
       warning_preventer.reset!
     end
 
-    c.after do |example|
-      warning_preventer.verify_example!(example)
+    c.after do
+      warning_preventer.verify_no_warnings!
     end
   end
 
@@ -24,4 +26,54 @@ RSpec.configure do |c|
 
   c.filter_run :focus
   c.run_all_when_everything_filtered = true
+  # opal
+  #c.example_status_persistence_file_path = "./spec/examples.txt"
+
+  c.define_derived_metadata :failing_on_appveyor do |meta|
+    meta[:pending] ||= "This spec fails on AppVeyor and needs someone to fix it."
+  end# opal -  if ENV['APPVEYOR']
 end
+
+module RSpec
+  module Support
+    module Spec
+      # Opal
+      # def self.setup_simplecov(&block)
+      #   # Simplecov emits some ruby warnings when loaded, so silence them.
+      #   old_verbose, $VERBOSE = $VERBOSE, false
+      #
+      #   return if ENV['NO_COVERAGE'] || RUBY_VERSION < '1.9.3'
+      #   return if RUBY_ENGINE != 'ruby' || RSpec::Support::OS.windows?
+      #
+      #   # Don't load it when we're running a single isolated
+      #   # test file rather than the whole suite.
+      #   return if RSpec.configuration.files_to_run.one?
+      #
+      #   require 'simplecov'
+      #   start_simplecov(&block)
+      # rescue LoadError
+      #   warn "Simplecov could not be loaded"
+      # ensure
+      #   $VERBOSE = old_verbose
+      # end
+      #
+      # def self.start_simplecov(&block)
+      #   SimpleCov.start do
+      #     add_filter "./bundle/"
+      #     add_filter "./tmp/"
+      #     add_filter do |source_file|
+      #       # Filter out `spec` directory except when it is under `lib`
+      #       # (as is the case in rspec-support)
+      #       source_file.filename.include?('/spec/') && !source_file.filename.include?('/lib/')
+      #     end
+      #
+      #     instance_eval(&block) if block
+      #   end
+      # end
+      # private_class_method :start_simplecov
+    end
+  end
+end
+
+RSpec::Matchers.define_negated_matcher :avoid_raising_errors, :raise_error
+RSpec::Matchers.define_negated_matcher :avoid_changing, :change
