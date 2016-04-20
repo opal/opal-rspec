5ed8e8f60b6ab957554cad2d01269ac9665fd7a0
Brady Wied <brady@bswtechconsulting.com>
2016-04-19 22:18:04 -0600

    Will need this again, but reflect spirit of change from https://github.com/opal/opal-rspec/commit/b61a56a9a583f62ac732959b1fe64430f91ceb86

--- opal/opal/rspec/fixes/rspec/support/ruby_features.rb
+++ opal/opal/rspec/fixes/rspec/support/ruby_features.rb
@@ -35,9 +35,8 @@ module RSpec
     module RubyFeatures
       module_function
 
-      # Weird behavior when optional_and_splat_args_supported? is false (which is the case on Opal) and required_kw_args_supported? is true, so forcing this to false
       def required_kw_args_supported?
-        false
+        true
       end
 
       def kw_args_supported?
@@ -74,6 +73,5 @@ module RSpec
         Module.method_defined?(:prepend) || Module.private_method_defined?(:prepend)
       end
     end
-
   end
 end
--- opal/opal/rspec/pre_require_fixes.rb
+++ opal/opal/rspec/pre_require_fixes.rb
@@ -1,1 +1,3 @@
 require_relative 'fixes/opal/compatibility'
+# RSpec code changes based on this
+require_relative 'fixes/rspec/support/ruby_features'
