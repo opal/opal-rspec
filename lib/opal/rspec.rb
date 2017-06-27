3010a5c9f329729e52714da1533b8316586bcc97
Elia Schito <elia@schito.me>
2017-06-28 01:50:34 +0200

    Move spec opts code generator to a helper

--- lib/opal/rspec.rb
+++ lib/opal/rspec.rb
@@ -38,3 +38,36 @@ stubs = [
 ]
 
 ::Opal::Config.stubbed_files.merge(stubs)
+
+
+module Opal
+  module RSpec
+    def self.spec_opts_code(spec_opts)
+      code = []
+      if spec_opts && !spec_opts.empty?
+        code << 'RSpec.configure do |config|'
+
+        if (match = /--(no-)?color\b/.match(spec_opts))
+          color_value = !match.captures[0]
+          # Have to use instance_variable_set because config.color= is designed to not allow overriding color once it's set, but
+          # we do not yet have true SPEC_OPTS parsing via RSpec config to get it initially set
+          code << "config.instance_variable_set(:@color, #{color_value})"
+        end
+
+        if (requires = spec_opts.scan(/--require \S+/)).any?
+          requires.map {|r| /--require (.*)/.match(r).captures[0]}.each do |req|
+            code << %{require "#{req}"}
+          end
+        end
+
+        if (match = /--format (\S+)/.match(spec_opts))
+          formatter = match.captures[0]
+          code << %{config.formatter = "#{formatter}"}
+        end
+
+        code << 'end'
+      end
+      code.join('; ')
+    end
+  end
+end
