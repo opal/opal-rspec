b86d722741e44924aa3c2bf62f2b8312d5f477a0
Elia Schito <elia@schito.me>
2017-07-15 14:55:06 +0200

    Fix constant lookup

--- opal/opal/rspec/fixes/rspec/core/notifications/summary_notification.rb
+++ opal/opal/rspec/fixes/rspec/core/notifications/summary_notification.rb
@@ -1,26 +1,21 @@
-module ::RSpec::Core::Notifications
-  # SummaryNotification is a Struct
-  SummaryNotification.class_eval do
-    def totals_line
-      summary = Formatters::Helpers.pluralize(example_count, "example")
-      # 2 string mutations
-      summary = summary + ", " + Formatters::Helpers.pluralize(failure_count, "failure")
-      # string mutation
-      summary = summary + ", #{pending_count} pending" if pending_count > 0
-      summary
+# string mutations
+::RSpec::Core::Notifications::SummaryNotification.class_eval do
+  def totals_line
+    summary = ::RSpec::Core::Formatters::Helpers.pluralize(example_count, "example")
+    summary += ", " + ::RSpec::Core::Formatters::Helpers.pluralize(failure_count, "failure")
+    summary += ", #{pending_count} pending" if pending_count > 0
+    summary
+  end
+
+  def fully_formatted(colorizer=::RSpec::Core::Formatters::ConsoleCodes)
+    formatted = "\nFinished in #{formatted_duration} " \
+                "(files took #{formatted_load_time} to load)\n" \
+                "#{colorized_totals_line(colorizer)}\n"
+
+    unless failed_examples.empty?
+      formatted += colorized_rerun_commands(colorizer) + "\n"
     end
 
-    def fully_formatted(colorizer=::RSpec::Core::Formatters::ConsoleCodes)
-      formatted = "\nFinished in #{formatted_duration} " \
-                  "(files took #{formatted_load_time} to load)\n" \
-                  "#{colorized_totals_line(colorizer)}\n"
-
-      unless failed_examples.empty?
-        # 2 string mutations
-        formatted = formatted + colorized_rerun_commands(colorizer) + "\n"
-      end
-
-      formatted
-    end
+    formatted
   end
 end
