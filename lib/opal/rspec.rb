c8f39fae455713e9966fed59936c1e65a0ced55b
Elia Schito <elia@schito.me>
2017-07-19 00:52:20 +0200

    Move stubs to actual files in a folder
    To shorten the CLI command needed by opal-rspec.

--- lib/opal/rspec.rb
+++ lib/opal/rspec.rb
@@ -1,10 +1,11 @@
 require 'opal'
-require 'opal/sprockets'
+require 'opal-sprockets'
 require 'opal/rspec/version'
 require 'opal/rspec/sprockets_environment'
 
 # Just register our opal code path with opal build tools
 Opal.append_path File.expand_path('../../../opal', __FILE__)
+Opal.append_path File.expand_path('../../../stubs', __FILE__)
 
 # Catch our git submodule included directories
 %w{rspec rspec-core rspec-expectations rspec-mocks rspec-support}.each do |gem|
@@ -13,32 +14,6 @@ end
 
 # Since we have better specs than before (and a script to deal with this), ignoring
 Opal::Config.dynamic_require_severity = :ignore
-
-stubs = [
-  'mutex_m', # Used with some threading operations but seems to run OK without this
-  'prettyprint',
-  'tempfile', # Doesn't exist in Opal
-  'diff/lcs',
-  'diff/lcs/block',
-  'diff/lcs/callbacks',
-  'diff/lcs/change',
-  'diff/lcs/hunk',
-  'diff/lcs/internals',
-  'test/unit/assertions',
-  # Opal doesn't have optparse, yet
-  'optparse',
-  'shellwords',
-  'socket',
-  'uri',
-  'drb/drb',
-  'cgi/util',
-  'minitest', # RSpec uses require to see if minitest is there, opal/sprockets won't like that, so stub it
-  'minitest/unit',
-  'minitest/assertions'
-]
-
-::Opal::Config.stubbed_files.merge(stubs)
-
 
 module Opal
   module RSpec
